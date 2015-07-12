ad489e9f12160823082ff6e63f158ebe9a885c0f
faktoreal <faktoreal@gmail.com>
2015-07-12 17:56:44 +0300

    MAN: 1) scss usage 2) cleaned code 3) 1st slider try 4) big_block ("Latin America") made small

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+
+    /* -------------  Slider For Programs  -------------- */
+    $('.bxslider').bxSlider({
+        //mode: 'horizontal',
+        useCSS: false,
+
+        mode: 'fade',
+        speed: 2000
+    });
+
+    /* -------------  Slider For Programs END -------------- */
+
+
+
+});
+
+
+
+
